1f5b6c0f595e74e0188c5fff60f59e55fcd783ed
Konstantin <konstantin56k@gmail.com>
2022-04-27 14:44:20 -0400

    fixed union function and added comments

--- quick_find_disjoint_set.js
+++ quick_find_disjoint_set.js
@@ -4,17 +4,23 @@ class UnionFind {
     constructor(size) {
         this.root = new Array(size).fill().map((char, idx) => char = idx)
     }
-
+    // Time O(1)
     find(node) {
         return this.root[node]
     }
-
+    /* When we connect one set to another we need to fine all node that 
+    connected to old root node ane reassign them to the new root node.
+    Time O(N) */
     union(node1, node2) {
         if (this.find(node1) !== this.find(node2)) {
-            this.root[node2] = this.find(node1)
+            for (let i = 0; i < this.root.length; i++) {
+                if (this.root[i] === this.find(node2)) {
+                    this.root[i] = this.find(node1)
+                }
+            }
         }
     }
-
+    // Time O(1)
     isConnected(node1, node2) {
         if (this.find(node1) === this.find(node2)) {
             return true
@@ -33,8 +39,11 @@ graph.union(6, 7);
 graph.union(3, 8);
 graph.union(8, 9);
 
-console.log(graph.isConnected(1, 4));
+console.log(graph.isConnected(2, 8));
 
-graph.union(1, 4);
+// 1-2-5-6-7-3-8-9
+graph.union(3, 7);
 
-console.log(graph.isConnected(1, 4));
+
+console.log(graph.isConnected(3, 7));
+console.log(graph.isConnected(2, 8));
